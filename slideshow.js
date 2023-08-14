12b6a4ce138f4b88a89dc5720c0b00f7c503f5bf
heelsboroharry <74061248+heelsboroharry@users.noreply.github.com>
2023-08-14 14:32:55 -0400

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,20 +1,23 @@
-// slideshow.js
-let slideIndex = 0;
-showSlides();
+// gallery.js
+let slideIndex = 1; // Start from the first slide
+showSlides(slideIndex);
 
-function showSlides() {
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
     let slides = document.getElementsByClassName("slide");
+    
+    if (n > slides.length) {
+        slideIndex = 1;
+    } else if (n < 1) {
+        slideIndex = slides.length;
+    }
     
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    
     slides[slideIndex - 1].style.display = "block";
-    
-    setTimeout(showSlides, 3000); // Change slide every 3 seconds
 }
